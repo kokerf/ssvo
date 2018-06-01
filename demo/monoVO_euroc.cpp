26465e87fe1ffc6e6ccaf677faec89e51f965c10
kokerf <kokerf@163.com>
2018-06-01 17:26:32 +0800

    modify dataset loader

---
+++ demo/monoVO_euroc.cpp
@@ -0,0 +1,45 @@
+#include "system.hpp"
+#include "dataset.hpp"
+#include "time_tracing.hpp"
+
+using namespace ssvo;
+
+int main(int argc, char *argv[])
+{
+    google::InitGoogleLogging(argv[0]);
+    LOG_ASSERT(argc == 3) << "\n Usage : ./monoVO_dataset config_file dataset_path";
+
+    System vo(argv[1]);
+    EuRocDataReader dataset(argv[2]);
+
+    ssvo::Timer<std::micro> timer;
+    const size_t N = dataset.leftImageSize();
+    for(size_t i = 0; i < N; i++)
+    {
+        const EuRocDataReader::Image image_data = dataset.leftImage(i);
+        LOG(INFO) << "=== Load Image " << i << ": " << image_data.path << ", time: " << std::fixed <<std::setprecision(7)<< image_data.timestamp << std::endl;
+        cv::Mat image = cv::imread(image_data.path, CV_LOAD_IMAGE_UNCHANGED);
+        if(image.empty())
+            continue;
+
+        timer.start();
+        vo.process(image, image_data.timestamp);
+        timer.stop();
+
+        double time_process = timer.duration();
+
+        double time_wait = 0;
+        if(i < N -1)
+            time_wait = (dataset.leftImage(i+1).timestamp - image_data.timestamp)*1e6;
+        else
+            time_wait = (image_data.timestamp - dataset.leftImage(i-1).timestamp)*1e6;
+
+        if(time_process < time_wait)
+            std::this_thread::sleep_for(std::chrono::microseconds((int)(time_wait - time_process)));
+    }
+
+    vo.saveTrajectoryTUM("trajectory.txt");
+    getchar();
+
+    return 0;
+}
--- include/dataset.hpp
+++ include/dataset.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <opencv2/opencv.hpp>
 
 namespace ssvo {
 
@@ -39,7 +40,7 @@ public:
                     std::vector<double> ground_truth(7);
                     for(int i = 0; i < 7; ++i)
                        ss >> ground_truth[i];
-                    ground_truth_.push_back(ground_truth);
+                    groundtruth_data_.push_back(ground_truth);
                 }
             }
         }
@@ -70,7 +71,7 @@ public:
     {
         if(!readItemByIndex(index, rgb_image, depth_image, timestamp))
             return false;
-        ground_truth = ground_truth_[index];
+        ground_truth = groundtruth_data_[index];
         return true;
     }
 
@@ -81,28 +82,122 @@ public:
     std::vector<double> timestamps_;
     std::vector<std::string> rgb_images_;
     std::vector<std::string> depth_images_;
-    std::vector<std::vector<double> > ground_truth_;
+    std::vector<std::vector<double> > groundtruth_data_;
 };
 
-class EuRocDataReader{
+class EuRocDataReader {
 public:
-    EuRocDataReader(const std::string image_path, const std::string csv_file)
+
+    struct Image{
+        double timestamp;
+        std::string path;
+    };
+
+    struct IMUData{
+        double timestamp;
+        std::array<double, 3> gyro; //! w_RS_S_[x,y,z]
+        std::array<double, 3> acc;  //! a_RS_S_[x,y,z]
+    };
+
+    struct GroundTruthData{
+        double timestamp;
+        std::array<double, 3> p; //! p_RS_R_[x,y,z]
+        std::array<double, 4> q; //! q_RS_[w,x,y,z]
+        std::array<double, 3> v; //! v_RS_R_[x,y,z]
+        std::array<double, 3> w; //! b_w_RS_S_[x,y,z]
+        std::array<double, 3> a; //! b_a_RS_S_[x,y,z]
+    };
+
+    EuRocDataReader(const std::string dataset_path, const bool info = false)
     {
-        std::string path = image_path;
-        size_t found = path.find_last_of("/\\");
-        if(found+1 != path.size())
-            path += path.substr(found,1);
+        std::string root_path = dataset_path;
+        size_t found = root_path.find_last_of("/\\");
+        if(found > root_path.size())
+        {
+            std::cerr << "EuRocDataReader::load, Please check the path: " + dataset_path << std::endl;
+            throw;
+        }
 
-        std::ifstream file_stream;
-        file_stream.open(csv_file, std::ifstream::in);
+        std::string divide_str = root_path.substr(found, 1);
+        if (found + 1 != root_path.size())
+            root_path += divide_str;
 
+        int code = 0;
+
+        //! for image0
+        std::string cam_path0 = root_path + "cam0" + divide_str;
+        std::string image_path0 = cam_path0 + "data" + divide_str;
+        code = loadCameraData(cam_path0, image_path0, left_);
+        if(code != 0)
+        {
+            std::cerr << "EuRocDataReader, Load cam0 with error " << code <<  ". Please check the cam0 path: " + cam_path0 << std::endl;
+            throw;
+        }
+
+        //! for image1
+        std::string cam_path1 = root_path + "cam1" + divide_str;
+        std::string image_path1 = cam_path1 + "data" + divide_str;
+        code = loadCameraData(cam_path1, image_path1, right_);
+        if(code != 0)
+        {
+            std::cerr << "EuRocDataReader, Load cam1 with error " << code <<  ". Please check the cam1 path: " + cam_path1 << std::endl;
+            throw;
+        }
+
+        //! for IMU
+        std::string imu_path = root_path + "imu0" + divide_str;
+        code = loadIMUData(imu_path, imu_);
+        if(code != 0)
+        {
+            std::cerr << "EuRocDataReader, Load imu with error " << code <<  ". Please check the imu path: " + imu_path << std::endl;
+            throw;
+        }
+
+        //! for groundtruth
+        std::string groundtruth_path = root_path + "state_groundtruth_estimate0" + divide_str;
+        code = loadGroundtruthData(groundtruth_path, groundtruth_);
+        if(code != 0)
+        {
+            std::cerr << "EuRocDataReader, Load groundtruth with error " << code <<  ". Please check the groundtruth path: " + groundtruth_path << std::endl;
+            throw;
+        }
+
+    }
+
+    const size_t leftImageSize() const { return left_.size(); }
+
+    const size_t rightImageSize() const { return right_.size(); }
+
+    const size_t imuSize() const { return imu_.size(); }
+
+    const size_t groundtruthSize() const { return groundtruth_.size(); }
+
+    const Image& leftImage(size_t idx) const { return left_.at(idx); }
+
+    const Image& rightImage(size_t idx) const { return right_.at(idx); }
+
+    const IMUData& imu(size_t idx) const { return imu_.at(idx); }
+
+    const GroundTruthData& groundtruth(size_t idx) const { return groundtruth_.at(idx); }
+
+private:
+
+    int loadCameraData(std::string root_path, std::string data_path, std::vector<Image> &images)
+    {
+        std::string camera_csv = root_path + "data.csv";
+
+        std::ifstream file_stream(camera_csv, std::ifstream::in);
+        if (!file_stream.is_open())
+            return -1;
+
+        //! image path
         std::string buffer;
         getline(file_stream, buffer);
-        while(getline(file_stream, buffer))
+        while (getline(file_stream, buffer))
         {
             size_t found = buffer.find_last_not_of(" \t\n\r");
-            if(found!=std::string::npos)
-                buffer.erase(found+1);
+            if (found != std::string::npos)
+                buffer.erase(found + 1);
             else
                 break;
 
@@ -112,38 +207,127 @@ public:
             getline(string_stream, time_buffer, ',');
             time_t time;
             std::istringstream(time_buffer) >> time;
-            double timestamp = time/1000000000.0;
             getline(string_stream, name_buffer);
-            std::string image_path = path+name_buffer;
-            timestamps_.push_back(timestamp);
-            images_.push_back(image_path);
 
+            Image image = { time * 1e-9, data_path + name_buffer};
+            images.push_back(image);
         }
         file_stream.close();
-        N = timestamps_.size();
 
-        if(N == 0)
-            std::cerr << "No item read! Please check csv file: " << csv_file << std::endl;
-        else
-            std::cerr << "Avaiable image items in dataset: " << N << std::endl;
+        //! config file
+        std::string camera_yaml = root_path + "sensor.yaml";
+
+
+        return 0;
     }
 
-    bool readItemByIndex(size_t index, std::string &rgb_image, double &timestamp) const
+    int loadIMUData(std::string path, std::vector<IMUData> &imu)
     {
-        if(index >= N)
+        //! csv
+        std::string imu_csv = path + "data.csv";
+        std::ifstream file_stream(imu_csv, std::ifstream::in);
+        if (!file_stream.is_open())
+            return -1;
+
+        std::string buffer;
+        getline(file_stream, buffer);
+        while (getline(file_stream, buffer))
         {
-            std::cerr << " Index(" << index << ") is out of scape, max should be (0~" << N - 1 << ")";
-            return false;
+            size_t found = buffer.find_last_not_of(" \t\n\r");
+            if (found != std::string::npos)
+                buffer.erase(found + 1);
+            else
+                break;
+
+            std::istringstream string_stream(buffer);
+            std::string time_buffer;
+            std::string imu_buffer;
+            getline(string_stream, time_buffer, ',');
+            time_t time;
+            std::istringstream(time_buffer) >> time;
+            getline(string_stream, imu_buffer);
+
+            IMUData data;
+            data.timestamp = time * 1e-9;
+            char dot;
+            std::istringstream(imu_buffer)
+                    >> data.gyro[0] >> dot
+                    >> data.gyro[1] >> dot
+                    >> data.gyro[2] >> dot
+                    >> data.acc[0] >> dot
+                    >> data.acc[1] >> dot
+                    >> data.acc[2];
+
+            assert(dot == ',');
+
+            imu.push_back(data);
         }
-        rgb_image = images_[index];
-        timestamp = timestamps_[index];
-        return true;
+        file_stream.close();
+
+        return 0;
     }
 
-public:
-    size_t N;
-    std::vector<double> timestamps_;
-    std::vector<std::string> images_;
+    int loadGroundtruthData(std::string path, std::vector<GroundTruthData> &groundtruth)
+    {
+        std::string ground_truth_csv = path + "data.csv";
+        std::ifstream file_stream(ground_truth_csv, std::ifstream::in);
+        if (!file_stream.is_open())
+            return -1;
+
+        std::string buffer;
+        getline(file_stream, buffer);
+        while (getline(file_stream, buffer))
+        {
+            size_t found = buffer.find_last_not_of(" \t\n\r");
+            if (found != std::string::npos)
+                buffer.erase(found + 1);
+            else
+                break;
+
+            std::istringstream string_stream(buffer);
+            std::string time_buffer;
+            std::string pose_buffer;
+            getline(string_stream, time_buffer, ',');
+            time_t time;
+            std::istringstream(time_buffer) >> time;
+            getline(string_stream, pose_buffer);
+
+            GroundTruthData data;
+            data.timestamp = time * 1e-9;
+            char dot;
+            std::istringstream(pose_buffer)
+                    >> data.p[0] >> dot
+                    >> data.p[1] >> dot
+                    >> data.p[2] >> dot
+                    >> data.q[0] >> dot
+                    >> data.q[1] >> dot
+                    >> data.q[2] >> dot
+                    >> data.q[3] >> dot
+                    >> data.v[0] >> dot
+                    >> data.v[1] >> dot
+                    >> data.v[2] >> dot
+                    >> data.w[0] >> dot
+                    >> data.w[1] >> dot
+                    >> data.w[2] >> dot
+                    >> data.a[0] >> dot
+                    >> data.a[1] >> dot
+                    >> data.a[2];
+
+            assert(dot == ',');
+
+            groundtruth.push_back(data);
+        }
+        file_stream.close();
+
+        return 0;
+    }
+
+private:
+
+    std::vector<Image> left_;
+    std::vector<Image> right_;
+    std::vector<IMUData> imu_;
+    std::vector<GroundTruthData> groundtruth_;
 };
 
 }
